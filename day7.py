b2ec59240def3846c8016f8b7c4805c9b243d4df
cppcho <cppcho.hk@gmail.com>
2023-12-10 23:21:46 +0900

    Solve day7 part1

---
+++ day7.py
@@ -0,0 +1,98 @@
+import re
+import functools
+import heapq
+import collections
+import enum
+
+LABELS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+Kind = enum.Enum(
+    "Kind",
+    [
+        "HIGH_CARD",
+        "ONE_PAIR",
+        "TWO_PAIR",
+        "THREE_OF_A_KIND",
+        "FULL_HOUSE",
+        "FOUR_OF_A_KIND",
+        "FIVE_OF_A_KIND",
+    ],
+)
+
+
+@functools.total_ordering
+class Hand:
+    def __init__(self, cards: str):
+        self.cards = cards
+
+    def __repr__(self):
+        return f"Hand({self.cards} {self.kind()})"
+
+    def kind(self):
+        kind_dict = collections.defaultdict(int)
+        for card in self.cards:
+            kind_dict[card] = kind_dict[card] + 1
+
+        if max(kind_dict.values()) == 5:
+            return Kind.FIVE_OF_A_KIND
+        if max(kind_dict.values()) == 4:
+            return Kind.FOUR_OF_A_KIND
+        if max(kind_dict.values()) == 3 and min(kind_dict.values()) == 2:
+            return Kind.FULL_HOUSE
+        if max(kind_dict.values()) == 3:
+            return Kind.THREE_OF_A_KIND
+        if max(kind_dict.values()) == 2 and len(kind_dict.values()) == 3:
+            return Kind.TWO_PAIR
+        if max(kind_dict.values()) == 2 and len(kind_dict.values()) == 4:
+            return Kind.ONE_PAIR
+        return Kind.HIGH_CARD
+
+    def _is_valid_operand(self, other):
+        return isinstance(other, Hand)
+
+    def __lt__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+
+        if self.kind() == other.kind():
+            for i in range(0, 5):
+                idx = LABELS.index(self.cards[i])
+                other_idx = LABELS.index(other.cards[i])
+                if idx != other_idx:
+                    return idx < other_idx
+            return False
+        else:
+            return self.kind().value < other.kind().value
+
+    def __eq__(self, other):
+        return self.cards == other.cards
+
+
+class Solver:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+        self.lines = []
+        with open(self.file_name, "r", encoding="utf-8") as file:
+            for line in file:
+                self.lines.append(line.strip())
+
+    def part_one(self):
+        hands = []
+
+        for line in self.lines:
+            cards, bid = line.split(" ")
+            heapq.heappush(hands, (Hand(cards), int(bid)))
+
+        return sum(heapq.heappop(hands)[1] * (i + 1) for i in range(len(hands)))
+
+    def part_two(self):
+        return
+
+
+if __name__ == "__main__":
+    example_solver = Solver("data/day7_input_example.txt")
+    solver = Solver("data/day7_input.txt")
+    print(f"Q1 (Example): {example_solver.part_one()}")
+    # print(f"Q2 (Example): {example_solver.part_two()}")
+    print(f"Q1: {solver.part_one()}")
+    # print(f"Q2: {solver.part_two()}")
